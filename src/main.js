9cc31e9cfc8103512e4ce6a65b747e9e6e472fe0
Dmitry Pozdnyakov <2903015@gmail.com>
2022-05-02 09:43:06 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const App = {
+    data() {
+        return {
+            title: 'Список заметок',
+            placeholder: 'Ввести название...',
+            inputValue: ''
+        }
+    },
+    methods: {
+        inputChange(event) {
+            console.log(event, event.data, event.target.value)
+            this.inputValue = event.target.value
+        }
+    },
+}
+const app = Vue.createApp(App)
+app.mount('#app')
+
