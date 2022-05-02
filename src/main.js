d248445d160f85742742ff056e86a4622efd25e1
Dmitry Pozdnyakov <2903015@gmail.com>
2022-05-02 11:30:38 +0300

    Часть 2

--- src/main.js
+++ src/main.js
@@ -3,15 +3,40 @@ const App = {
         return {
             title: 'Список заметок',
             placeholder: 'Ввести название...',
-            inputValue: ''
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2', 'Заметка 3']
         }
     },
     methods: {
         inputChange(event) {
             console.log(event, event.data, event.target.value)
             this.inputValue = event.target.value 
+        },
+        addNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ""
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        deleteNote(index) {
+            this.notes.splice(index, 1)
         }
     },
+    computed: {
+        doubleCount() {
+            return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length == 0) {
+                //alert("Название не может быть пустым!")
+            }
+        }
+    }
 }
 const app = Vue.createApp(App)
 app.mount('#app')
